49852e060fba240e7c3d1c1c4bd8601ea9ba4bdd
shonenada <shonenada@gmail.com>
2013-12-19 15:00:29 +0800

    add two model

---
+++ drift/bottles/model.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from drift.app import db
+
+
+class Bottle(db.Model):
+
+    BOTTLE_STATE = ('normal', 'deleted')
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(250))
+    created = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    state = db.Column(db.Enum('bottle_state', *BOTTLE_STATE))
+
+    def __init__(self, content, user):
+        self.user = user
+        self.content = content
+        self.state = 'normal'
