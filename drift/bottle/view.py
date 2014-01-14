e9ae1380bfb911e9d924bb7bb28f0b2980f0ccf2
shonenada <shonenada@gmail.com>
2014-01-14 22:35:36 +0800

    add bottles handler

--- drift/bottle/view.py
+++ drift/bottle/view.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8-
 from flask import Blueprint, render_template
-from flask import request, redirect, url_for, jsonify
+from flask import request, redirect, url_for, jsonify, json
 from flask.ext.login import login_required, current_user
 
 from drift.app import rbac, db
@@ -11,7 +11,7 @@ from drift.bottle.form import ThrowForm
 bottle_app = Blueprint('bottle', __name__)
 
 
-@bottle_app.route('/bottles')
+@bottle_app.route('/leadin')
 @login_required
 def leadin():
     throw_form = ThrowForm()
@@ -29,6 +29,17 @@ def river():
     return render_template('bottle/bottles.html', bottles=bottles)
 
 
+@bottle_app.route('/river/bottles')
+@login_required
+def bottles():
+    page = int(request.args.get('page', 1))
+    bottles = Bottle.query.filter_by(state='normal').paginate(
+        page = page,
+        per_page = 20
+    ).items
+    return json.dumps([bottle.serialization for bottle in bottles])
+
+
 @bottle_app.route('/throw', methods=['POST'])
 @login_required
 def throw():
@@ -41,6 +52,7 @@ def throw():
         return jsonify(success=True, message=u'成功扔出一支瓶子', categore='notice')
     if form.errors:
         return jsonify(success=False, messages=form.errors.values())
+
 
 @bottle_app.route('/pick', methods=['POST'])
 @login_required
